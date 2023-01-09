712743d0f723983fbc422abd0d9fc90342c18ba0
Danila EV <dalnas@mail.ru>
2023-01-09 17:03:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = new int[]{5,8,9,12,55,32};
+        SalesManager salesManager = new SalesManager(sales);
+        System.out.println(salesManager.max());
+
+    }
+}
