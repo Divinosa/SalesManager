69d9f64832ccfd6b8d749722b7ed9e51d4ebc875
Danila EV <dalnas@mail.ru>
2023-01-11 16:48:08 +0300

    add medium

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,23 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = sales[0];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int medium() {
+        int medium = 0;
+        for (int sale : sales) {
+            medium = medium + sale;
+        }
+        medium = (medium - (this.max() + this.min())) / (this.sales.length - 2);
+        return medium;
+    }
 }
